04fa00f4e8f787a4c26e7dc2c9042a8545d07137
ritikrathore0812@gmail.com <ritikrathore0812@gmail.com>
2024-03-10 14:00:24 +0530

    update

--- myfile.java
+++ myfile.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, jenkins this is my first");
+        System.out.println("Hello, jenkins this is my first tq");
     }
 }
