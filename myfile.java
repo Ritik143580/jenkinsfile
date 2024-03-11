e1dac0a11e5321189c9545433487c117b1b99f98
Ritik143580 <ritikrathore0812@gmail.com>
2024-03-11 08:13:11 +0530

    my final commit

--- myfile.java
+++ myfile.java
@@ -1,5 +1,5 @@
 public class  myfile {
     public static void main(String[] args) {
-        System.out.println("Hello, 00 jenkins this is my first tq ooooo");
+        System.out.println("Hello, 00 jenkins this is my first java project");
     }
 }
